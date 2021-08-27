4d2429dfd140e66b04b77dd97d6710d1ad094d1a
kelli-alan <alankelli64@gmail.com>
2021-08-27 13:19:34 -0400

    Added SearchBehavior interface and implemented LinearSearch

---
+++ strategy/LinearSearch.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+
+/**
+ * Searches an ArrayList sequentially from index 0 to the end of the list for a specified item
+ * @author Kelli Alan
+ */
+public class LinearSearch implements SearchBehavior {
+
+  /**
+   * Searches an ArrayList linearly, starting at index 0, for a case insensitive, specified String
+   * @param data the ArrayList of Strings to search 
+   * @param item the String to look for in the given ArrayList
+   * @return true if the String is found in the ArrayList; false if the String is not found
+   */
+  public boolean contains(ArrayList<String> data, String item) {
+    boolean wasFound = false;
+    for(int i = 0; i < data.size(); ++i) {
+      if(data.get(i).equalsIgnoreCase(item)) {
+        wasFound = true;
+        break;
+      }
+    }
+    return wasFound;
+    
+  }
+}
---
+++ strategy/SearchBehavior.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+/**
+ * Implement a searching algorithm for an ArrayList
+ * @author Kelli Alan
+ */
+public interface SearchBehavior {
+  /**
+   * Determines if a specified item is in a given ArrayList
+   * @param data the ArrayList of Strings to search
+   * @param item the String to search for in the given ArrayList
+   * @return true if the String is found in the ArrayList; false if the String is not found
+   */
+  public boolean contains(ArrayList<String> data, String item);
+}
