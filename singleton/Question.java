4793e3ce3d3834a32c7c729f7aa0da651111e2db
kelli-alan <alankelli64@gmail.com>
2021-10-01 22:05:09 -0400

    Added all functionality

--- singleton/Question.java
+++ singleton/Question.java
@@ -1,24 +1,41 @@
 package singleton;
 
+/**
+ * A trivia question with answer choices and a correct answer
+ * @author Kelli Alan
+ */
 public class Question {
   private String question;
   private String[] answers;
   private int correctAnswer;
 
-  public Question(String question, String ans1, String ans2, String ans3, String ans4, int correctAnswer) {
-  
+  public Question(String question, String ans1, String ans2, String ans3, String ans4, 
+                      int correctAnswer) {
+    this.question = question;
+    this.answers = new String[4];
+    this.answers[0] = ans1;
+    this.answers[1] = ans2;
+    this.answers[2] = ans3;
+    this.answers[3] = ans4;
+    this.correctAnswer = correctAnswer;
   }
 
   public String toString() {
-    return "";
+    String question = this.question + "\n";
+    for(int i = 0; i < answers.length; ++i) {
+      question += " " + (i+1) + ". " + answers[i] + "\n";
+    }
+    return question;
   }
 
   public boolean isCorrect(int userAnswer) {
-    return false;
+    userAnswer--;
+    return userAnswer == this.correctAnswer;
   }
 
   public String getCorrectAnswer() {
-    return "";
+    
+    return "The correct answer is " + answers[this.correctAnswer];
   }
   
 }
--- singleton/TriviaGame.java
+++ singleton/TriviaGame.java
@@ -3,6 +3,7 @@ import java.util.Random;
 import java.util.Scanner;
 import java.util.ArrayList;
 
+
 public class TriviaGame {
   private static TriviaGame triviaGame;
   private int score;
@@ -11,18 +12,52 @@ public class TriviaGame {
   private ArrayList<Question> questions;
 
   private TriviaGame() {
-
+    score = 0;
+    rand = new Random();
+    reader = new Scanner(System.in);
+    questions = DataLoader.getTriviaQuestions();
   }
 
   public static TriviaGame getInstance() {
-    return null;
+    if (triviaGame == null) {
+      triviaGame = new TriviaGame();
+    }
+    return triviaGame;
   }
 
   public void play() {
-
+    
+    while(true) {
+      if(playRound()) {
+        score++;
+      }
+      
+      System.out.println("(P)lay or (Q)uit: ");
+      String choice = reader.nextLine();
+      if(choice.equalsIgnoreCase("Q")) {
+        break;
+      
+      }
+    }
+    System.out.println("You won " + this.score + " games!\nGoodbye");
   }
 
   private boolean playRound() {
+    int questionNum = rand.nextInt(questions.size());
+    System.out.print(questions.get(questionNum).toString());
+
+    System.out.println("Enter answer: ");
+    int userAnswer = reader.nextInt();
+    reader.nextLine();
+
+    if (userAnswer < 1 || userAnswer > 4) {
+      System.out.println("Invalid answer choice");
+    }else if (questions.get(questionNum).isCorrect(userAnswer)) {
+      System.out.println("YAY! You got it right!");
+      return true;
+    }
+
+    System.out.println("You got it wrong!\n" + questions.get(questionNum).getCorrectAnswer());
     return false;
   }
 }
